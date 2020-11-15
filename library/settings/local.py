299b9e0540423e73a933151c6e568ecbc9350bf9
Esteban Fajardo <estebanfajardo1101@gmail.com>
2020-11-14 22:57:39 -0500

    create project

---
+++ library/settings/local.py
@@ -0,0 +1,21 @@
+from .base import *
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', #'django.db.backends.sqlite3'
+        'NAME': 'library', #'NAME': BASE_DIR / 'db.sqlite3'
+        'USER': 'esteban',
+        'PASSWORD': 'Sopra2020*-*',
+        'HOST': '192.168.5.85',
+        'PORT': '3306',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
